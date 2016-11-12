9f8ab23898c974747cdfbb36531b99e8ebf3b4bc
BoryaMogila <bora.mog@gmail.com>
2016-11-12 13:25:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 require("./app/app.js");
 
-module.exports = {
-    entry: './index.js',
-    target: 'node',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'backend.js'
-    },
-    module      : {
-        loaders: [
-            {
-                loader : 'babel',
-                exclude: /node_modules/,
-                query: {
-                    plugins: ['transform-runtime'],
-                    presets: ['stage-0', "react", "es2015-node5"],
-                }
-            },
-            {test: /\.css$/, loader: "style-loader!css-loader"},
-        ]
-    }
-
+    externals: nodeModules,
+    plugins: [
+        new webpack.IgnorePlugin(/\.(css|less)$/),
+        new webpack.BannerPlugin('require("source-map-support").install();',
+            { raw: true, entryOnly: false })
+    ],
+    devtool: 'sourcemap'
+};
