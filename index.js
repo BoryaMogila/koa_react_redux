48dd7cdb448dcffeeda3b6c094fbbfcac7227d98
BoryaMogila <bora.mog@gmail.com>
2016-11-12 13:22:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 require("./app/app.js");
 
-var webpack = require('webpack');
-var path = require('path');
-var fs = require('fs');
-
-
-var nodeModules = {};
-fs.readdirSync('node_modules')
-    .filter(function(x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function(mod) {
-        nodeModules[mod] = 'commonjs ' + mod;
-    });
+module.exports = {
+    entry: './index.js',
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'backend.js'
+    },
+    module      : {
+        loaders: [
+            {
+                loader : 'babel',
+                exclude: /node_modules/,
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['stage-0', "react", "es2015-node5"],
+                }
+            },
+            {test: /\.css$/, loader: "style-loader!css-loader"},
+        ]
+    }
 
