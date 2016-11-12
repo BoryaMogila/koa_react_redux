db1ec618e839211cefc4e2115b6c49b473ad87aa
BoryaMogila <bora.mog@gmail.com>
2016-11-12 12:50:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 require("./app/app.js");
 
-app.get("/users", function(req, res, next){
-User.find(function(err, users){
-   if (err) { return next(err); }
-   // ваша логіка
-   });
-});
-app.use(function(err, req, res, next) {
-   res.status(err.status || 500);
-   res.render('error', {
-      message: err.message,
-      error: err
-   });
-});
+app.use(function(ctx, next){
+	try{
+		   await next();
+      } catch (error){
+		   ctx.status = 500;
+		   await ctx.render(“error”, {error});
+      }
+})
 
