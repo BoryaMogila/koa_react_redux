a4835a461461be1629e8da5cc0ebabd51ec28934
BoryaMogila <bora.mog@gmail.com>
2016-11-12 12:05:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ import React, {Component} from 'react';
 import {connect} from ‘react-redux’;
 import someAsyncAction from ‘./someAsyncAction’
 
-
 class MyComponent extends Component{
-
-
 	static fetchData({dispatch, params, props}){
-	const promiseArr = [
-            	dispatch(someAsyncAction (/*параметри для екшена*/));
-        	];
-        	return Promise.all(promiseArr);
-}
-componentDidMount(){
-	const {props} = this,
-	{dispatch, params} = props;
-	MyComponent.fetchData({dispatch, params, props});
-}
+		const promiseArr = [
+			dispatch(someAsyncAction (/*параметри для екшена*/));
+			];
+			return Promise.all(promiseArr);
+	}
+	componentDidMount(){
+		const {props} = this,
+		{dispatch, params} = props;
+		MyComponent.fetchData({dispatch, params, props});
+	}
