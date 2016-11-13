005f712f7d9c3be2527cfd861513b94558007aeb
BoryaMogila <bora.mog@gmail.com>
2016-11-13 22:55:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 require("./app/app.js");
 
-import Koa from'koa';
-import errorHeandler from '.errorHeandler'
-import reactApp from './reactApp'
-const app = new Koa();
-
-app.use(errorHeandler);
-
-app.use(reactApp);
-
-app.listen(3000);
