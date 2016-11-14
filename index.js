2803165b2e8379ea4d42a84a5867b96ae05f79f0
BoryaMogila <bora.mog@gmail.com>
2016-11-14 17:14:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 require("./app/app.js");
 
+export default const fetchComponentData = (dispatch, components, params, props) => {
+   return Promise.all(
+       components
+           .filter(
+                component => typeof component.fetchData === 'function'
+           )
+           .map(component => component.fetchData(
+                {dispatch, params, props}
+           )));
+}
